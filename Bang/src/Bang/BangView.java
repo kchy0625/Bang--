b903a688baaf04e950ad867518f3c978aef6a9a1
Young <Young@116.68.47.59>
2019-10-18 18:05:37 +0900

    .

---
+++ Bang/src/Bang/BangView.java
@@ -0,0 +1,17 @@
+package Bang;
+
+public class BangView {
+	public static void main(String[] args) {
+		
+		String titleMsg = "Bang";
+		String startMsg = "1. 게임 시작";
+		String howMsg = "2. 게임 방법";
+		String endMsg = "3. 끝"; 
+		
+		System.out.println(titleMsg);
+		System.out.println(startMsg);
+		System.out.println(howMsg);
+		System.out.println(endMsg);
+
+	}
+}
